c1535d5c935b195de7b3b4db5d8bad5769304007
bobaec <bobaec@sfu.ca>
2020-02-19 00:43:55 -0800

    add auth to react, fix directories and gitignore

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCYxNLnw1IWv8RH6mCfxSTLUFTxS-G4Hsg",
+  authDomain: "cmpt470stocks.firebaseapp.com",
+  databaseURL: "https://cmpt470stocks.firebaseio.com",
+  projectId: "cmpt470stocks",
+  storageBucket: "cmpt470stocks.appspot.com",
+  messagingSenderId: "1049287622156",
+  appId: "1:1049287622156:web:6518b75151cafd00687829",
+  measurementId: "G-ZQF33DHQ42"
+};
