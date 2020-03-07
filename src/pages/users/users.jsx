b898cc1dd0d4009a7bcd6d557336535690776a7c
Steven Lee <steven.lee0126@gmail.com>
2020-03-07 02:13:17 -0800

    add basic design for profile

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -6,7 +6,11 @@ class UsersPage extends React.Component {
 		    <div className = "mainContent">
 				<div className = "wrapper">
 					<center><h1>Your Profile</h1></center>
-					<h3>Hello, {this.props.user.displayName}</h3>
+					<h3>{this.props.user.displayName}</h3>
+					<div className="profile">
+						<img src={this.props.user.photoURL} class="profile_img"/><br/>
+						<button className="general_button" >Edit</button>
+					</div>
 				</div>
 			</div>
 		);
