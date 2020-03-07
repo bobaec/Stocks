8607e9ff2b7f2446cd32e0b06a0a984c6348f439
Steven Lee <steven.lee0126@gmail.com>
2020-03-07 08:17:51 -0800

    apply bootstrap in profile and users

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,89 +1,94 @@
 import React from "react";
 import {
 	BrowserRouter as Router,
-	Route,
-	Switch,
 	Link,
-	Redirect
   } from "react-router-dom";
+
+import {
+	Table,
+	Col,
+	Row,
+	Container,
+	Image
+} from 'react-bootstrap';
+
 class Profile extends React.Component {
 	render() {
 		return (
 		    <div className = "mainContent">
 				<div className = "wrapper">
-					<center><h1>Your Profile</h1></center>
-					<h3>{this.props.user.displayName}</h3>
-					<div className="profile">
-						<img src={this.props.user.photoURL} class="profile_img"/>
-						<div>
-							<button class="general_button">Edit</button>
-						</div>
-						<br/>
-						<h4>Information</h4>
-						<div className="user_information">
-							<table>
-								<tr>
-									<td>Name</td>
-									<td>{this.props.user.displayName}</td>
-								</tr>
-								<tr>
-									<td>Email</td>
-									<td>{this.props.user.email}</td>
-								</tr>
-							</table>
-						</div>
-						<br/>
-						<div className="fav_stock_summary">
-							<h4>Favourite Stocks</h4>
-							<table id="stock_table">
-								<thead>
-									<th>Stock</th>
-									<th>Last Price</th>
-									<th>Change</th>
-									<th>% Change</th>
-								</thead>
-								<tr>
-									<td>Alphabet Inc Class A</td>
-									<td>1,295.74 USD</td>
-									<td style={{
-										color: -1 < 0 ? "red" : "green"
-										}}>
-										−19.02%
-										<i className = "fa fa-angle-up"/>
-									</td>
-									<td id="stock_table">1.45%</td>
-								</tr>
-								<tr>
-									<td>Microsoft Corporation</td>
-									<td>161.57 USD</td>
-									<td style={{
-										color: -1 < 0 ? "red" : "green"
-										}}>
-										−4.7%
-										<i className = "fa fa-angle-up"/>
-									</td>
-									<td>2.83%</td>
-								</tr>
-								<tr>
-									<td>Intel Corporation</td>
-									<td>55.77 USD</td>
-									<td style={{
-										color: -1 < 0 ? "red" : "green"
-										}}>
-										−999.02%
-										<i className = "fa fa-angle-up"/>
-									</td>
-									<td>69.6%</td>
-								</tr>
-								<tr>
-									<td><Link to="/browse" style={{color:"inherit"}}>+ Add New</Link></td>
-								</tr>
-							</table>
-						</div>
-					</div>
+					<center><h2>Your Profile</h2></center>
+					<h5>{this.props.user.displayName}</h5><br/>
+					<Container>
+						<Col xs={12} md={12} className="profile">
+							<Image src={this.props.user.photoURL} roundedCircle width={150}/>
+							<div>
+								<button class="general_button">Edit</button>
+							</div>
+							<br/>
+							<h5>Information</h5>
+							<div className="user_information">
+								<Table  bordered variant="dark">
+									<tr>
+										<td>Name</td>
+										<td>{this.props.user.displayName}</td>
+									</tr>
+									<tr>
+										<td>Email</td>
+										<td>{this.props.user.email}</td>
+									</tr>
+								</Table>
+							</div>
+							<br/>
+							<div className="fav_stock_summary">
+								<h5>Favourite Stocks</h5>
+								<Col xs={12} md={12}>
+									<Table bordered variant="dark" id="stock_table">
+										<thead>
+											<th>Stock</th>
+											<th>Last Price</th>
+											<th>Change</th>
+										</thead>
+										<tr>
+											<td>Alphabet Inc Class A</td>
+											<td>1,295.74 USD</td>
+											<td style={{
+												color: -1 < 0 ? "red" : "green"
+												}}>
+												−19.02%
+												<i className = "fa fa-angle-up"/>
+											</td>
+										</tr>
+										<tr>
+											<td>Microsoft Corporation</td>
+											<td>161.57 USD</td>
+											<td style={{
+												color: -1 < 0 ? "red" : "green"
+												}}>
+												−4.7%
+												<i className = "fa fa-angle-up"/>
+											</td>
+										</tr>
+										<tr>
+											<td>Intel Corporation</td>
+											<td>55.77 USD</td>
+											<td style={{
+												color: -1 < 0 ? "red" : "green"
+												}}>
+												−999.02%
+												<i className = "fa fa-angle-up"/>
+											</td>
+										</tr>
+										<tr>
+											<td colSpan="4"><Link to="/browse" style={{color:"inherit"}}>+ Add New</Link></td>
+										</tr>
+									</Table>
+								</Col>
+							</div>
+						</Col>
+					</Container>
 				</div>
 			</div>
-			
 		);
 	}
 }
--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+	Col,
+	Container,
+	Card,
+	CardDeck,
+	ListGroup,
+	Row
+} from 'react-bootstrap';
+import stock_img from '../img/image.png'
 
 class UsersPage extends React.Component {
 	render() {
@@ -6,41 +15,37 @@ class UsersPage extends React.Component {
 		    <div className = "mainContent">
 				<div className = "wrapper">
 					<center><h1>Users</h1></center>
-					<div className="users">
-						<br/>
-						<table className="users_table">
-							<thead>
-								<th>Name</th>
-								<th># favourite stocks</th>
-								<th>Last Logged In</th>
-							</thead>
-							<tr>
-								<td>Sana</td>
-								<td>2</td>
-								<td>Mar 1, 2020</td>
-							</tr>
-							<tr>
-								<td>Naeune</td>
-								<td>2</td>
-								<td>Mar 1, 2020</td>
-							</tr>
-							<tr>
-								<td>Bobby</td>
-								<td>2</td>
-								<td>Mar 1, 2020</td>
-							</tr>
-							<tr>
-								<td>JYP</td>
-								<td>2</td>
-								<td>Mar 1, 2020</td>
-							</tr>
-							<tr>
-								<td>Parasite</td>
-								<td>2</td>
-								<td>Mar 1, 2020</td>
-							</tr>
-						</table>
-					</div>
+					<Container className="users">
+						<Row style={{marginTop: "10px"}}>
+							<CardDeck>
+								<Col>
+								<Card>
+									<Card.Img variant="top" src={stock_img} />
+									<Card.Body>
+										<Card.Title>{this.props.user.displayName}</Card.Title>
+									</Card.Body>
+								</Card>
+								</Col>
+								<Col>
+								<Card>
+									<Card.Img variant="top" src={stock_img} />
+									<Card.Body>
+										<Card.Title>{this.props.user.displayName}</Card.Title>
+									</Card.Body>
+								</Card>
+								</Col>
+								<Col>
+								<Card>
+									<Card.Img variant="top" src={stock_img} />
+									<Card.Body>
+										<Card.Title>{this.props.user.displayName}</Card.Title>
+									</Card.Body>
+								</Card>
+								</Col>
+							</CardDeck>
+						</Row>
+
+					</Container>
 				</div>
 			</div>
 		);
