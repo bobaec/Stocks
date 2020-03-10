a38e2b292f3c81b8e5216e6ac912b9659a159b6e
Alex Elesin <alex_elesin@sfu.ca>
2020-03-10 15:11:01 -0700

    WIP User functions

--- server/models/user.js
+++ server/models/user.js
@@ -21,4 +21,45 @@ const UserSchema = new Schema({
     ]
 });
 
-module.exports = mongoose.model('User', UserSchema, "user");
+const User = mongoose.model('User', UserSchema, "user");
+module.exports = User
+
+// Add new user
+exports.addNewUser = function (req, res) {
+    new User(req.body).save(function (err) {
+        if (err) {
+            res.status(400).send('Unable to save a new user to database');
+            console.log("Failed to save user");
+            console.log(err);
+        } else {
+            res.status(200).send("Successfully added new user");
+        }
+    });
+};
+
+exports.getuserById = function (req, res, id) {
+    var objectId = mongoose.Types.ObjectId(id);   // Convert string to objectId
+    User.find({_id: objectId});
+};
+
+// Get user by name
+exports.getUserByName = function (req, res, name) {
+    User.find({name: name});
+};
+
+// Get all users
+exports.getAllUsers = function (req, res) {
+    User.find(function (err, data) {
+        console.log(data);
+    });
+
+
+    // User.find({}).exec(function (err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.send(500, err);
+    //     }
+    //     console.log(data);
+    //     return res.send(200, data);
+    // });
+};
