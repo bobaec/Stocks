40121e5c971afe3f5186e5bb21d31df4825d38ac
Bobae <bobaec@sfu.ca>
2020-03-24 00:18:41 -0700

    display users from backend to frontend

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ import {
 } from 'react-bootstrap';
 
 class MainPage extends React.Component {
+
 	render() {
 		return (			
 			<div className = "mainContent">
--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -7,14 +7,70 @@ import {
 } from 'react-bootstrap';
 // import stock_img from '../img/image.png'
 
+const axios = require('axios');
+
+var allUsers;
+
+// testing in localhost
+axios.get('http://localhost:8001/user/all').then(function(response){
+	allUsers = response.data
+	for (let i = 0; i < allUsers.length; i++) {
+		console.log(allUsers[i])
+	}
+})
+
+function tableGenerate() {
+	var htmlAdd = "";
+	for (let i in allUsers) {
+		htmlAdd += 
+		"<Table responsive variant = 'dark' className = 'users_table'>" + 
+			"<thead>" + 
+				"<th>Name</th>" + 
+				"<th>Email</th>" + 
+				"<th>Stocks</th>" +
+			"</thead>" + 
+			"<tbody>" + 
+			"<tr>" + 
+				"<td>" + 
+					allUsers[i].name + 
+				"</td>"+
+				"<td>"+ 
+					allUsers[i].email + 
+				"</td>"+
+				"<td>" +
+					"2" + 
+				"</td>"+
+			"</tr>" + 
+			"</tbody>" + 
+		"</Table>"
+	}
+	console.log("aa");
+	document.getElementById("generate").innerHTML += (htmlAdd);
+}
+
+// use when deploying live
+// async function getUser() {
+// 	try {
+// 		const response = await axios.get('/api/v1/user/all');
+// 		console.log(response);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
+// getUser();
+
 class UsersPage extends React.Component {
+	componentDidMount() {
+		tableGenerate();
+	}
 	render() {
 		return (
 		    <div className = "mainContent">
 				<div className = "wrapper">
 					<center><h4>Users</h4></center>
 					<Container className="users">
-						<Row style={{marginTop: "10px"}}>
+						<Row style={{marginTop: "10px"}} id = "generate">
+
 							<Table responsive variant="dark" className="users_table">
 							<thead>
 								<th></th>
