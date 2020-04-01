8356d2bf60e5ade634d6924795f71fc92156326d
Bobae <bobaec@sfu.ca>
2020-04-01 15:20:40 -0700

    add cors to express config

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,11 @@
 // require express
 const express = require('express');
 const path    = require('path');
+var cors = require('cors');
 
 // create our router object
 const app = express.Router();
-
+app.use(cors());
 /**
  * Controllers (route handlers).
  */
