99a21c1a960be2b2e3f46b1d0dac5b5287a832c3
Steven Lee <steven.lee0126@gmail.com>
2020-04-05 19:47:22 -0700

    remove static fake data on profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -35,51 +35,6 @@ class Profile extends React.Component {
 									</tr>
 								</Table>
 							</div>
-							<br/>
-							<div className="fav_stock_summary">
-								<Col xs={12} md={12}>
-									<Table bordered variant="dark" id="stock_table">
-										<thead>
-											<th>Stock</th>
-											<th>Last Price</th>
-											<th>Change</th>
-										</thead>
-										<tr>
-											<td>Alphabet Inc Class A</td>
-											<td>1,295.74 USD</td>
-											<td style={{
-												color: -1 < 0 ? "red" : "green"
-												}}>
-												−19.02%
-												<i className = "fa fa-angle-up"/>
-											</td>
-										</tr>
-										<tr>
-											<td>Microsoft Corporation</td>
-											<td>161.57 USD</td>
-											<td style={{
-												color: -1 < 0 ? "red" : "green"
-												}}>
-												−4.7%
-												<i className = "fa fa-angle-up"/>
-											</td>
-										</tr>
-										<tr>
-											<td>Intel Corporation</td>
-											<td>55.77 USD</td>
-											<td style={{
-												color: -1 < 0 ? "red" : "green"
-												}}>
-												−999.02%
-												<i className = "fa fa-angle-up"/>
-											</td>
-										</tr>
-										<tr>
-											<td colSpan="4"><Link to="/stocks" style={{color:"inherit"}}>+ Add New</Link></td>
-										</tr>
-									</Table>
-								</Col>
-							</div>
 						</Col>
 					</Container>
 				</div>
